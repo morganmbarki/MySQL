5da59329a2b9cb6dae86ce4dedc4accf406eb60a
unknown <morgan.mbarki@gmail.com>
2019-12-06 14:01:35 +0100

    exercice 2

--- script.sql
+++ script.sql
@@ -5,7 +5,3 @@ CREATE DATABASE `languages`;
 CREATE DATABASE IF NOT EXISTS `languages`;
 --...........................................................Partie1 Exercice2
 CREATE DATABASE `webDevelopment` CHARACTER SET 'utf8';
---...........................................................Partie1 Exercice3
-CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET 'utf8';
---...........................................................Partie1 Exercice4
-DROP DATABASE `languages`;
