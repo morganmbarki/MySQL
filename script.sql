bbae22554b6f3b30c9285e7388ebf97d5da0f8f2
unknown <morgan.mbarki@gmail.com>
2019-12-06 13:59:35 +0100

    exercice 1

---
+++ script.sql
@@ -0,0 +1,11 @@
+--...........................................................Partie1 Exercice1
+--Création d'une base de donnée languages
+CREATE DATABASE `languages`;
+--Création d'une base de donnée languages si elle n'existe pas (meilleure solution)
+CREATE DATABASE IF NOT EXISTS `languages`;
+--...........................................................Partie1 Exercice2
+CREATE DATABASE `webDevelopment` CHARACTER SET 'utf8';
+--...........................................................Partie1 Exercice3
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET 'utf8';
+--...........................................................Partie1 Exercice4
+DROP DATABASE `languages`;
